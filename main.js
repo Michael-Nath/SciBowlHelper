57a4836ddedab834789b5f3bd83ba17813960ab2
Michael <mnath10@stuy.edu>
2020-08-25 17:00:04 -0400

    added some arg handling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const { token, prefix } = require("./config.json");
 
 const client = new Discord.Client();
 
@@ -8,9 +8,29 @@ client.once("ready", () => {
 });
 
 client.on("message", (msg) => {
-	if (msg.content === "ping") {
-		msg.reply("pong!");
+	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+	const args = msg.content.slice(prefix.length).trim().split(" ");
+	const command = args.shift().toLowerCase();
+	if (command === "args-info") {
+		if (!args.length) {
+			return msg.channel.send(
+				`You didn't provide any arguments, ${msg.author}!`
+			);
+		}
+		if (args.includes("biology")) {
+			return msg.channel.send("BIOLOGY IS COOL I AGREE.");
+		}
+		msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
 	}
+	// if (msg.content.startsWith(`${prefix}ping`)) {
+	// 	msg.channel.send("Pong!");
+	// } else if (msg.content.startsWith(`${prefix}greet`)) {
+	// 	msg.channel.send(`${msg.author}, Hello!`);
+	// } else if (msg.content.startsWith(`${prefix}server`)) {
+	// 	msg.channel.send(
+	// 		`Welcome to ${msg.guild.name}!\nTotal Members: ${msg.guild.memberCount}`
+	// 	);
+	// }
 });
 
-client.login();
+client.login(token).catch((err) => console.error(err));
