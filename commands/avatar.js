f2d5ff59369376d8b64b9a1b6429ba5f61899ee4
Michael <mnath10@stuy.edu>
2020-08-25 20:11:57 -0400

    worked on the help command

---
+++ commands/avatar.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "avatar",
+    description: "Avatar",
+    aliases: ["icon", 'pfp'],
+    
+}
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
+
 const commandFiles = fs
 	.readdirSync("./commands")
 	.filter((file) => file.endsWith(".js"));
@@ -24,22 +25,45 @@ client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) {
+	const command =
+		client.commands.get(commandName) ||
+		client.commands.find(
+			(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+		);
+	if (!command) {
 		message.channel.send(
 			"Sorry! This command is not in the list of available commands."
 		);
 		return;
 	}
-
-	const command = client.commands.get(commandName);
 	if (command.args && !args.length) {
 		let reply = `You didn't provide any arguments, ${message.author}!`;
 		if (command.usage) {
-			reply +=
-				`\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
 		}
 		return message.channel.send(reply);
 	}
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+		if (now < expirtationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(
+				`please wait ${timeLeft.toFixed(
+					1
+				)} more second(s) before reusing the \`${command.name}\` command.`
+			);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
 	try {
 		command.get(command).execute(message, args);
 	} catch (error) {
