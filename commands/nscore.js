ad5e5d2d9a2ea74c24b1ba1fc93f1c9d750651a5
Michael <mnath10@stuy.edu>
2020-09-27 01:13:13 -0400

    worked on the 'wrong' function

--- commands/nscore.js
+++ commands/nscore.js
@@ -1,9 +1,14 @@
 var greenPoints = 0;
 var redPoints = 0;
+let bonusRound;
+let tossRound;
+let buzzerTimeout;
+let currentBuzzing;
 function resetVariables() {
-	toss = false;
+	tossRound = false;
+	bonusRound = false;
 	currentBuzzing = "";
-	let buzzerTimeout;
+	buzzerTimeout = null;
 }
 
 function start(message, args) {
@@ -23,7 +28,7 @@ function roundEnd(message, args) {
 }
 
 function tossUp(message, args) {
-	toss = true;
+	tossRound = true;
 	// after 5 seconds, toss up round should end
 	buzzerTimeout = message.client.setTimeout(() => {
 		message.channel.send("Toss up is over!");
@@ -32,9 +37,13 @@ function tossUp(message, args) {
 	return message.channel.send("Toss up has started!");
 }
 
+function bonus(message, args) {
+	bonusRound = true;
+}
+
 function buzz(message, args) {
 	// if a toss up round hasn't started, return an error message
-	if (!toss) {
+	if (!tossRound) {
 		return message.channel.send("Error: Tossup round hasn't started yet.");
 	}
 	// if someone has already buzzed, no one else should be buzzing.
@@ -51,12 +60,60 @@ function buzz(message, args) {
 }
 
 function right(message, args) {
-	if (currentBuzzing == "green") {
-		greenPoints += 4;
-	} else if (currentBuzzing == "red") {
-		redPoints += 4;
-	} else {
-		return message.channel.send("Contact **Michael Nath**");
+	if (tossRound) {
+		tossRound = false;
+		if (currentBuzzing == "green") {
+			greenPoints += 4;
+		} else if (currentBuzzing == "red") {
+			redPoints += 4;
+		} else {
+			return message.channel.send("Contact **Michael Nath**");
+		}
+	} else if (args[1] == "b") {
+		bonusRound = false;
+		if (currentBuzzing == "green") {
+			greenPoints += 10;
+		} else if (currentBuzzing == "red") {
+			redPoints += 10;
+		} else {
+			return message.channel.send("Contact **Michael Nath**");
+		}
+	}
+	return message.channel.send("Points have successfully been added.");
+}
+
+function wrong(message, args) {
+	if (tossRound) {
+		// switches the team that's now buzzing
+		if (currentBuzzing == "green") {
+			currentBuzzing = "red";
+		} else if (currentBuzzing == "red") {
+			currentBuzzing = "green";
+		} else {
+			return message.channel.send("Contact **Michael Nath**");
+		}
+		message.channel.send("Opposing team, get ready...");
+		// effectively gives people three seconds to get ready to buzz.
+		let i = 2;
+		bufferTimeout = message.client.setInterval(() => {
+			if (i != 0) {
+				message.channel.send(i);
+			} else {
+				message.client.clearTimeout(bufferTimeout);
+				message.channel.send("You may buzz now!");
+			}
+			i -= 1;
+		}, 1000);
+		// after three seconds are over, the other team will oficially have 5 seconds to buzz in.
+		buzzerTimeout = message.client.setTimeout(() => {
+			message.channel.send("Toss up is over!");
+			toss = false;
+		}, 8050);
+		return buzzerTimeout;
+	}
+
+	if (bonusRound) {
+		return message.channel.send("Wait for moderator to end round.");
 	}
 }
 
@@ -81,10 +138,10 @@ module.exports = {
 				return buzz(message, args);
 			case "right":
 				return right(message, args);
-			// case "wrong":
-			// 	return wrong(message, args);
-			// case "bo":
-			// 	return bonus(message, args);
+			case "wrong":
+				return wrong(message, args);
+			case "bo":
+				return bonus(message, args);
 		}
 	},
 };
